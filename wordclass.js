455cab6ad008a36f62d2a25216f957879f1d928e
JasperLiebert <jasperliebert@students.berkeley.net>
2019-05-23 11:13:56 -0700

    Create wordclass.js

---
+++ wordclass.js
@@ -0,0 +1,12 @@
+class letter {
+	constructor (name, clicked, x, y, xVel, yVel, grav) {
+  	this.name = name;
+    this.x = x;
+    this.y = y;
+    this.clicked = clicked;
+    this.xVel = xVel;
+    this.yVel = yVel;
+    this.grav = grav;
+
+  }
+}
